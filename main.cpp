42b956a7770dd18f1d474b8c705fa953816077ab
niccolonardelli <niccolo.nardelli@marconibari.edu.it>
2024-11-07 09:02:23 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main()
+ {
+    int n, d, nd;
+    cout << "inserisci numero" << endl;
+    cin >>n;
+    nd = 0;
+    for (d =1; d <=n ; d = d+1)
+    {
+        if(n%d==0)
+        {
+            cout << d << " è divisore di " << n << endl;
+            nd = nd + 1;
+        }
+        else 
+        {
+
+        }
+    }
+    cout << n << " ha " << nd << " divisori." << endl;
+    if (nd ==2)
+    {
+        cout << n << " è un numero primo" << endl;
+    }
+    else
+    {
+        cout << n << " non è un numero primo" << endl;
+    }
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
